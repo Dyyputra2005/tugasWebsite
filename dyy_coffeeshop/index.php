2c739e3b6c1c1c648d24653a2c39a5007ca1ec0f
ADY PUTRA <dyyptra@gmail.com>
2026-06-08 00:43:29 +0700

    Update index.php

--- dyy_coffeeshop/index.php
+++ dyy_coffeeshop/index.php
@@ -3,13 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coffee Shop</title>
+    <title>Titik Temu Cofeeshop</title>
 
     <!-- Fonts -->
-     <link rel="preconnect" href="https://fonts.googleapis.com">
-     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Satisfy&display=swap" rel="stylesheet">
-
+     <<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Lobster&family=Lobster+Two:ital,wght@0,400;0,700;1,400;1,700&family=Satisfy&display=swap" rel="stylesheet">
      <!-- My Style -->
       <link rel="stylesheet" href="css/style.css">
 
@@ -22,7 +21,7 @@
 <body>
     <!-- Navbar start -->
      <nav class="navbar">
-        <a href="#" class="navbar-logo">dyy<span>coffeeshop</span>.</a>
+        <a href="#" class="navbar-logo">Titik<span> Temu</span>.</a>
 
         <div class="navbar-nav">
             <a href="#home">Home</a>
@@ -45,7 +44,10 @@
       <section class="hero" id="home">
         <main class="content">
             <h1>Mari Nikmati Secangkir <span>Kopi</span></h1>
-            <p>SAMLEKOM MAMANG!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing 
+                elit. Minima voluptate libero perferendis aperiam unde,
+                 quam est? Distinctio, molestias amet. Ratione beatae ullam 
+                 sit suscipit a atque quasi est placeat assumenda!</p>
              <a href="#" class="cta">Beli Sekarang</a>    
         </main>
       </section>
@@ -55,10 +57,33 @@
        <section id="about" class="about">
         <h2>Tentang <span>Kami</span></h2>
         <div id="row">
-            about
+            <div class="row">
+                <img src="img/Tentang Kami.jpg" alt="Tentang Kami">
+            </div>
+            <div class="content">
+                <h3>Kenapa memilih kopi kami?</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur 
+                    adipisicing elit. Perferendis porro excepturi 
+                    eligendi explicabo inventore fuga neque doloribus 
+                    similique facere aperiam natus hic possimus minima 
+                    repellat perspiciatis totam, accusantium, blanditiis soluta?</p>
+                <p>Lorem ipsum, dolor sit amet consectetur 
+                    adipisicing elit. Perferendis porro excepturi 
+                    eligendi explicabo inventore fuga neque doloribus 
+                    similique facere aperiam natus hic possimus minima 
+                    repellat perspiciatis totam, accusantium, blanditiis soluta?</p>
+            </div>
         </div>
        </section>
        <!-- about section end -->
+
+       <!-- menu section start -->
+        <section id="menu" class="menu">
+            <h2><span>Menu</span>Kami</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam alias
+                 aliquid aperiam architecto dolore reiciendis?</p>
+        </section>
+        <!-- menu section end -->
 
      <!-- Javascript -->
       <script src="js/script.js"></script>
