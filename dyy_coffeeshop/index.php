380b67864939e05e0d8f979f7923b3d22826dd39
William George Simbolon <simbolonwilliam0@gmail.com>
2026-06-07 16:53:55 +0700

    Update index.php

--- dyy_coffeeshop/index.php
+++ dyy_coffeeshop/index.php
@@ -45,10 +45,7 @@
       <section class="hero" id="home">
         <main class="content">
             <h1>Mari Nikmati Secangkir <span>Kopi</span></h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Minima voluptate libero perferendis aperiam unde,
-                 quam est? Distinctio, molestias amet. Ratione beatae ullam 
-                 sit suscipit a atque quasi est placeat assumenda!</p>
+            <p>SAMLEKOM MAMANG!</p>
              <a href="#" class="cta">Beli Sekarang</a>    
         </main>
       </section>
