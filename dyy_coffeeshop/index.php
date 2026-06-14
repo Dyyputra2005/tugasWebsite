6ee8dd7f4dcc7e0cacde2c67be91fdcbabb42616
ADY PUTRA <dyyptra@gmail.com>
2026-06-14 15:59:56 +0700

    Update index.php

--- dyy_coffeeshop/index.php
+++ dyy_coffeeshop/index.php
@@ -6,7 +6,7 @@
     <title>Titik Temu Cofeeshop</title>
 
     <!-- Fonts -->
-     <<link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Lobster&family=Lobster+Two:ital,wght@0,400;0,700;1,400;1,700&family=Satisfy&display=swap" rel="stylesheet">
      <!-- My Style -->
@@ -43,12 +43,10 @@
      <!-- hero section start -->
       <section class="hero" id="home">
         <main class="content">
-            <h1>Mari Nikmati Secangkir <span>Kopi</span></h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing 
-                elit. Minima voluptate libero perferendis aperiam unde,
-                 quam est? Distinctio, molestias amet. Ratione beatae ullam 
-                 sit suscipit a atque quasi est placeat assumenda!</p>
-             <a href="#" class="cta">Beli Sekarang</a>    
+            <h1>Ruang Nyaman,<span>Kopi Istimewa</span></h1>
+            <p>Lebih dari sekadar tempat singgah, 
+                kami menghadirkan secangkir semangat untuk mengawali harimu dan ruang hangat untuk berbagi cerita. 
+                Temukan rasa favoritmu yang diracik khusus oleh barista kami.</p>    
         </main>
       </section>
       <!-- Hero section end -->
@@ -56,8 +54,8 @@
       <!-- about section start -->
        <section id="about" class="about">
         <h2>Tentang <span>Kami</span></h2>
-        <div id="row">
-            <div class="row">
+        <div class="row">
+            <div class="about-img">
                 <img src="img/Tentang Kami.jpg" alt="Tentang Kami">
             </div>
             <div class="content">
@@ -82,7 +80,46 @@
             <h2><span>Menu</span>Kami</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam alias
                  aliquid aperiam architecto dolore reiciendis?</p>
+            <div class="row">
+             <div class="menu-card">
+                <img src="img/Latte.jpg" alt="Latte" class="menu-card-img">
+                <h3 class="menu-card-title">Latte</h3>
+                <p class="menu-card-price">IDR 20k</p>
+             </div>
+             <div class="menu-card">
+                <img src="img/Latte.jpg" alt="Latte" class="menu-card-img">
+                <h3 class="menu-card-title">Latte</h3>
+                <p class="menu-card-price">IDR 20k</p>
+             </div>
+             <div class="menu-card">
+                <img src="img/Latte.jpg" alt="Latte" class="menu-card-img">
+                <h3 class="menu-card-title">Latte</h3>
+                <p class="menu-card-price">IDR 20k</p>
+             </div>
+             <div class="menu-card">
+                <img src="img/Latte.jpg" alt="Latte" class="menu-card-img">
+                <h3 class="menu-card-title">Latte</h3>
+                <p class="menu-card-price">IDR 20k</p>
+             </div>
+             <div class="menu-card">
+                <img src="img/Latte.jpg" alt="Latte" class="menu-card-img">
+                <h3 class="menu-card-title">Latte</h3>
+                <p class="menu-card-price">IDR 20k</p>
+             </div>
+             <div class="menu-card">
+                <img src="img/Latte.jpg" alt="Latte" class="menu-card-img">
+                <h3 class="menu-card-title">Latte</h3>
+                <p class="menu-card-price">IDR 20k</p>
+             </div>
+             <div class="menu-card">
+                <img src="img/Latte.jpg" alt="Latte" class="menu-card-img">
+                <h3 class="menu-card-title">Latte</h3>
+                <p class="menu-card-price">IDR 20k</p>
+             </div>
+           </div>     
         </section>
+
+        
         <!-- menu section end -->
 
      <!-- Javascript -->
