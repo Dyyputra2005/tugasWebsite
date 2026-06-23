9d6de9d89356c23329ff82aac726bf73df034caf
ADY PUTRA <dyyptra@gmail.com>
2026-06-24 00:54:54 +0700

    Enhance interactivity with shopping card and modal

--- dyy_coffeeshop/js/script.js
+++ dyy_coffeeshop/js/script.js
@@ -11,12 +11,21 @@ onclick = () => {
 // toggle class active untuk search form
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
+const sc = document.querySelector('#shopping-card-button');
 
 document.querySelector('#search-button').onclick = (e) => {
     searchForm.classList.toggle('active');
     searchBox.focus();
     e.preventDefault();
  };
+
+//  toggle class active shopping card
+const shoppingcard = document.querySelector('.shopping-card');
+document.querySelector('#shopping-card-button').onclick= (e) => {
+    shoppingcard.classList.toggle('active');
+    e.preventDefault();
+};
+
 
 
 //klik diluar elemen
@@ -30,4 +39,32 @@ document.addEventListener('click', function(e) {
     if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
         searchForm.classList.remove('active');
     }
+    if(!sc.contains(e.target) && !shoppingcard.contains(e.target)) {
+        shoppingcard.classList.remove('active');
+    }
 });
+
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = 'none';
+  }
+};
